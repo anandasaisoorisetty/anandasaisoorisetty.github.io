81e9137f5660977f353c89dedd2e2be84bffaf18
anandasaisoorisetty <anandasaisoorisetty@gmail.com>
2025-02-22 12:44:32 +0530

    Added finance.jpg to ads popup and enabled on all screens

--- src/app/components/ads/ads.component.ts
+++ src/app/components/ads/ads.component.ts
@@ -12,20 +12,13 @@ export class AdsComponent implements OnInit {
   showPopup = false;
 
   ngOnInit() {
-    // Always show popup on mobile screens
-    this.checkScreenSize();
-    // Debug: Log to confirm this runs
+    // Always show popup on all screens
+    this.showPopup = true;
     console.log('AdsComponent ngOnInit triggered, showPopup:', this.showPopup);
-  }
-
-  checkScreenSize() {
-    // Show popup if screen width is 768px or less (mobile/small screens)
-    this.showPopup = window.innerWidth <= 768;
   }
 
   closePopup() {
     this.showPopup = false;
-    // Debug: Confirm close works
     console.log('Popup closed');
   }
 }
