deb56be5af6f1eaa77327256f2d4f75e8131afa7
anandasaisoorisetty <anandasaisoorisetty@gmail.com>
2025-03-08 18:53:03 +0530

    neccasarry changes made in home component

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter, Routes } from '@angular/router';
-import { HomeComponent } from './app/components/home/home.component'; 
+import { HomeComponent } from './app/components/home/home.component';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker'; 
 
 const routes: Routes = [
   { path: '', component: HomeComponent }
 ];
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]
 }).catch(err => console.error(err));
